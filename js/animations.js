421c128882609432bf0518ef22d369e18e8de016
albertantony911 <66047048+albertantony911@users.noreply.github.com>
2024-10-11 09:54:26 +0530

    morning update

--- js/animations.js
+++ js/animations.js
@@ -114,39 +114,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     );
   });
     
-    // Define your blob shapes
-  const shapes = [
-    "M45272.5,22384.3A409.448,409.448,0,1,1,45272.5,21564.5A409.448,409.448,0,1,1,45272.5,22384.3Z", // Initial circle
-    "M45700,21950C45880,21980 46300,22300 46000,22600C45800,22750 45400,22850 45300,22650C45200,22500 45100,22300 45100,22150C45100,21900 45300,21800 45700,21950Z" // Example blob shape
-  ];
-
-  // GSAP shape animation
-  gsap.to("#gradientBG", {
-    duration: 1,
-    attr: { d: shapes[1] }, // Animation target shape
-    repeat: -1,
-    yoyo: true,
-    ease: "power1.inOut"
-  });
-
-  // Function to get random movement values
-  function getRandomMovement(max) {
-    return (Math.random() - 0.5) * 2 * max + 'vh'; // Random value between -max and +max
-  }
-
-  // GSAP movement animation with random direction
-  function animateRandomMovement() {
-    gsap.to("#gradientBG", {
-      duration: 1,
-      x: getRandomMovement(1.5), // Random x movement within -50 to +50 pixels
-      y: getRandomMovement(1.5), // Random y movement within -50 to +50 pixels
-      ease: "power1.inOut",
-      onComplete: animateRandomMovement // Repeat animation with new random values
-    });
-  }
-
-  animateRandomMovement(); // Start the random movement animation
-
 
 
 
@@ -642,5 +609,4 @@ window.addEventListener('load', function() {
   
   
 });
-
 
