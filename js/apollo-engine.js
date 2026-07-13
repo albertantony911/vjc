59d37d49cac0e18ac3ace3df99f9c93627996ec7
albertantony911 <66047048+albertantony911@users.noreply.github.com>
2026-07-13 17:44:36 +0530

    trial button update

---
+++ js/apollo-engine.js
@@ -0,0 +1,20 @@
+(function() {
+  var nocache = Math.random().toString(36).substring(7);
+  var script = document.createElement('script');
+  script.src = 'https://assets.apollo.io/js/apollo-inbound.js?nocache=' + nocache;
+  script.async = true;
+  
+  script.onload = function() {
+    try {
+      window.ApolloInbound.forms.init({ appId: '6a4b71010953ed001c11dfbe' });
+    } catch (err) {
+      console.error('[Apollo] Error initializing form builder:', err);
+    }
+  };
+  
+  script.onerror = function() {
+    console.error('[Apollo] Failed to load form builder script');
+  };
+  
+  document.head.appendChild(script);
+})();
